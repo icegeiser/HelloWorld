15a8b6d68a9e9075614d037ac62d1f58836cdc41
Leandro Teixeira <leandro.teixeira@instramed.com.br>
2021-06-14 15:26:06 -0300

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example.helloworld;
+//https://www.youtube.com/watch?v=H_XxH66lm3U
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Intellij Idea!");
+    }
+
+}
